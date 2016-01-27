0da18c6bff7d0e262af6bd41f0066531e5f8e825
Toray Altas <toray.altas@gmail.com>
2016-01-26 16:54:16 -0800

    Front end now working.

--- app/api/api/v1/meta_data.rb
+++ app/api/api/v1/meta_data.rb
@@ -12,9 +12,9 @@ class API::V1::MetaData < Grape::API
 
       # params[:urls]
       #test (need more time to write real tests!)
-      urls = ['https://www.pinterest.com/pin/349380883571292829/','https://www.facebook.com/shortcutapp/?fref=ts','http://www.goodreads.com/book/show/3273.Moloka_i','http://www.movietickets.com/movie/mid/163129']
+      # urls = ['https://www.pinterest.com/pin/349380883571292829/','https://www.facebook.com/shortcutapp/?fref=ts','http://www.goodreads.com/book/show/3273.Moloka_i','http://www.movietickets.com/movie/mid/163129']
 
-      MetaInfo.get_meta_infos(urls)
+      MetaInfo.get_meta_infos(params[:urls])
       # get meta data
     end
 
--- app/assets/javascripts/meta_info/search.js
+++ app/assets/javascripts/meta_info/search.js
@@ -4,17 +4,34 @@
 
 $(function() {
 
-    console.log('ihihih');
+
     $('#search').click(function () {
         var urls = $('#urls_in').val().split('\n');
+
+
+        // clear output data
+        $('#output_data').html('')
+        $('#loading').show()
 
         $.ajax({
             url: '/api/v1/meta_info',
             type: 'post',
             data: {urls: urls},
             dataType: 'json',
-            success: function(data) { console.log(data)},
-            error: function(data) { console.log(data)}
+            success: function(data) {
+                $.each(data, function(i,val){
+                    $('#output_data').append("<tr><td>"+val.os_type+"</td><td>"+val.original_url+"</td><td>"+val.app_name+"</td><td>"+val.app_store_id+"</td><td>"+val.package+"</td> <td>"+val.url+"</td> </tr>")
+                    $('#loading').hide()
+
+
+                });
+
+
+
+            },
+            error: function(data) { console.log('something went wrong, show some output to reflect')
+                $('#loading').hide()
+            }
         });
 
         //$.each(arrayOfLines, function (index, item) {
